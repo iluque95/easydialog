31a7fccc619eec002a24fd26a827b8f607215c85
WinDOveeR <marcos.nieto.98@gmail.com>
2019-11-21 18:00:15 +0100

    Amplified main

--- call_registry_test.cpp
+++ call_registry_test.cpp
@@ -9,12 +9,21 @@ int main()
     cout << "Testing call_registry..." << endl;
 
     call_registry cr;
+    if (cr.es_buit()) {
+        cout << "El call_registry está buit..." << endl;
+    } else {
+        cout << "Houston, we have a problem!" << endl;
+    }
 
     cout << "Registring call..." << endl;
+
     cr.registra_trucada(661635963);
+    cout << "El número de trucades actual és: " << cr.num_trucades(661635963) << endl;
 
     cout << "Assigning name to last call..." << endl;
     cr.assigna_nom(661635963, "Itiel");
+
+    cout << "El nom del telefon 661635963 és: " << cr.nom(661635963) << endl;
 
     cout << "Registring random calls..." << endl;
 
@@ -22,6 +31,7 @@ int main()
     {
         util::Random R;
         cr.registra_trucada(R(100000000,999999999));
+        cout << "El número actual d'entrades és: " << cr.num_entrades() << endl;
     }
 
     cout << "End of testing call_registry..." << endl;
