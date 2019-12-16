e697e9cff2e7c789b9b3a821191d66d314968881
WinDOveeR <marcos.nieto.98@gmail.com>
2019-12-16 22:13:20 +0100

    First version of dialog

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -3,4 +3,23 @@
 void dialog::dialog(easy_dial &easy, const string &input,
             vector<string> &answers, nat &numtelf) throw()
 {
-}
+
+    answers.push_back(easy.inici());
+    for (nat i=0; i < input.size(); ++i){
+
+        
+        try
+        {
+            answers.push_back(easy.seguent(input[i]));
+            numtelf = easy.num_telf();
+        }
+        catch(const std::exception& e)
+        {
+            numtelf = 0;
+            answers.clear();
+            answers.push_back(e);
+        }        
+
+    }
+
+}   
