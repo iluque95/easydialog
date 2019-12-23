7dbeae9d2cb5d3c94c68c5c7e54fb443df1f7113
iluque95 <iludi22@hotmail.es>
2019-12-23 15:20:03 +0100

    Improvements with errors and controlling enums filters.

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -1,25 +1,50 @@
 #include "../incl/dialog.hpp"
 
 void dialog::dialog(easy_dial &easy, const string &input,
-            vector<string> &answers, nat &numtelf) throw()
+                    vector<string> &answers, nat &numtelf) throw()
 {
 
     answers.push_back(easy.inici());
-    for (nat i=0; i < input.size(); ++i){
 
-        
+    nat i=0;
+    bool err = false;
+
+    while (i < input.size() and not err)
+    {
+
         try
         {
-            answers.push_back(easy.seguent(input[i]));
-            numtelf = easy.num_telf();
+            if (input[i] == phone::DELETECHAR)
+            {
+                answers.push_back(easy.anterior());
+            }
+            else if (input[i] == phone::ENDCHAR)
+            {
+                answers.push_back(easy.seguent(phone::ENDPREF));
+            }
+            else
+            {
+                answers.push_back(easy.seguent(input[i]));
+            }
+
+            try
+            {
+                numtelf = easy.num_telf();
+            }
+            catch(error e)
+            {
+                numtelf = 0;
+            }
+
         }
-        catch(const std::exception& e)
+        catch (error e)
         {
             numtelf = 0;
             answers.clear();
-            answers.push_back(e);
-        }        
+            answers.push_back(e.mensaje());
+            err = true;
+        }
 
+        ++i;
     }
-
-}   
+}
